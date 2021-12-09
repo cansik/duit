9b58debd9443bfc895fbbf4429514ca517e8cde2
Florian Bruggisser <florian@bildspur.ch>
2021-12-09 09:36:44 +0100

    added setup

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ It is based on the ideas of [cansik/bildspur-base](https://github.com/cansik/bil
 
 ## Example
 
+To create a gui by code, create a new class with the fields you need.
+
 ```python
 class Config:
     def __init__(self):
@@ -13,3 +15,44 @@ class Config:
         self.year = DataModel(2021) | NumberAnnotation("Year", 2000, 2050)
         self.temperature = DataModel(30.2) | SliderAnnotation("Temperature", 0, 40)
 ```
+
+And use the open3d gui package to display them:
+
+```python
+init_open3d_registry()
+
+config = Config()
+config.age.value = 10
+
+app = o3d.visualization.gui.Application.instance
+app.initialize()
+
+win = DemoWindow(config)
+
+app.run()
+```
+
+## Installation
+To install simbi with all dependencies call pip like this:
+
+```bash
+pip install "simbi[all]"
+```
+
+It is also possible to only install certain packages (for example for the open3d gui):
+
+```bash
+pip install "simbi[open3d]"
+```
+
+### Development
+
+To develop it is recommended to clone this repository and install the dependencies like this:
+
+```bash
+# in the visiongraph directory
+pip install -e ".[all]"
+```
+
+## About
+Copyright (c) 2021 Florian Bruggisser
---
+++ setup.py
@@ -0,0 +1,63 @@
+from typing import List
+
+from setuptools import setup, find_packages
+
+required_packages = find_packages(exclude=["test", "examples"])
+
+BASE_NAME = "__required__"
+ALL_NAME = "all"
+
+
+def parse_requirements():
+    extras = {}
+
+    with open('requirements.txt') as f:
+        lines = f.read().splitlines()
+
+    extra_name = BASE_NAME
+    extra_items: List[str] = []
+
+    for line in [line.strip() for line in lines if line != ""]:
+        if line.startswith("# extra"):
+            extras[extra_name] = extra_items
+            extra_items: List[str] = []
+
+            tokens = line.split(" ")
+            extra_name = tokens[2]
+
+        elif line.startswith("#"):
+            pass
+        elif line.startswith("-"):
+            pass
+        else:
+            extra_items.append(line)
+
+    # add last group
+    extras[extra_name] = extra_items
+
+    # extract base packages
+    install = extras.pop(BASE_NAME)
+
+    # create all group
+    all_reqs = list(extras.values())
+    extras[ALL_NAME] = []
+    for reqs in all_reqs:
+        extras[ALL_NAME] += reqs
+
+    return install, extras
+
+
+install_required, extras_required = parse_requirements()
+
+setup(
+    name="simbi",
+    version='0.1.0',
+    packages=required_packages,
+    url='https://github.com/cansik/simbi',
+    license='MIT License',
+    author='Florian Bruggisser',
+    author_email='github@broox.ch',
+    description='Simbi is a toolkit with helpful code for python.',
+    install_requires=install_required,
+    extras_require=extras_required
+)
