edb336357199de94e84d48654e5a1b03e4ac50eb
Florian Bruggisser <github@broox.ch>
2022-05-19 15:07:05 +0200

    updated version

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ long_description = (current_dir / "README.md").read_text()
 
 setup(
     name="duit",
-    version='0.1.0',
+    version='0.1.1',
     packages=required_packages,
     url='https://github.com/cansik/duit',
     license='MIT License',
