e363d90163458127239d0f48518ddc62dcb18e12
Florian Bruggisser <github@broox.ch>
2022-12-05 00:54:55 +0100

    added text input to slider

--- duit/ui/tk/properties/SliderProperty.py
+++ duit/ui/tk/properties/SliderProperty.py
@@ -5,24 +5,43 @@ from customtkinter.windows.widgets.core_widget_classes import CTkBaseClass
 
 from duit.ui.annotations.SliderAnnotation import SliderAnnotation
 from duit.ui.tk.TkFieldProperty import TkFieldProperty
+from duit.ui.tk.widgets.CTkNumberEntry import CTkNumberEntry
 
 
 class SliderProperty(TkFieldProperty[SliderAnnotation]):
     def create_field(self, master) -> CTkBaseClass:
-        int_variable = ctk.IntVar()
-        field = ctk.CTkSlider(master, variable=int_variable,
+        frame = ctk.CTkFrame(master)
+
+        data_variable = ctk.IntVar() if isinstance(self.model.value, int) else ctk.DoubleVar()
+        field = ctk.CTkSlider(frame,
+                              variable=data_variable,
                               from_=self.annotation.limit_min,
-                              to=self.annotation.limit_max)
+                              to=self.annotation.limit_max,
+                              width=140
+                              )
+        field.grid(row=0, column=0, padx=5, pady=0)
+
+        number_entry = CTkNumberEntry(frame,
+                                      self.model.value,
+                                      self.annotation.limit_min,
+                                      self.annotation.limit_max,
+                                      width=80)
+        number_entry.grid(row=0, column=1, padx=5, pady=0)
 
         def on_dm_changed(value):
-            int_variable.set(value)
+            data_variable.set(value)
+            number_entry.value = value
 
         def on_ui_changed(event):
-            self.model.value = int_variable.get()
+            self.model.value = data_variable.get()
+
+        def on_entry_changed(event):
+            self.model.value = number_entry.value
 
         self.model.on_changed.append(on_dm_changed)
         field.configure(command=on_ui_changed)
+        number_entry.on_changed(on_entry_changed)
 
         self.model.fire_latest()
 
-        return field
+        return frame
