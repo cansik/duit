eac9caed9454bdae28f12f3ff436707d16512272
Florian Bruggisser <github@broox.ch>
2022-12-15 14:35:01 +0100

    added tabs for sections

--- duit/ui/tk/TkPropertyPanel.py
+++ duit/ui/tk/TkPropertyPanel.py
@@ -1,12 +1,15 @@
 import logging
 import typing
-from typing import Union
+from typing import Union, Optional
 
 import customtkinter as ctk
 
+from duit.collections.Stack import Stack
 from duit.ui.BasePropertyPanel import BasePropertyPanel
 from duit.ui.PropertyRegistry import UI_PROPERTY_REGISTRY
 from duit.ui.annotations import find_all_ui_annotations
+from duit.ui.annotations.container.EndSectionAnnotation import EndSectionAnnotation
+from duit.ui.annotations.container.StartSectionAnnotation import StartSectionAnnotation
 from duit.ui.tk.TkFieldProperty import TkFieldProperty
 
 
@@ -15,14 +18,29 @@ class TkPropertyPanel(BasePropertyPanel, ctk.CTkFrame):
         BasePropertyPanel.__init__(self)
         ctk.CTkFrame.__init__(self, master, corner_radius=0)
 
-        self.grid_columnconfigure(1, weight=1)
+        self.tabview: Optional[ctk.CTkTabview] = None
+        self.general_tab: Optional[ctk.CTkFrame] = None
+
+        self._init_tab_view()
+
+    def _init_tab_view(self):
+        self.tabview = ctk.CTkTabview(self)
+        self.tabview.pack(padx=5, pady=5)
+
+        self.general_tab = self.tabview.add("General")
+        self.general_tab.grid_columnconfigure(1, weight=1)
 
     def _clean_widgets(self):
         for widget in self.winfo_children():
             widget.destroy()
 
+        self._init_tab_view()
+
     def _create_panel(self):
         self._clean_widgets()
+
+        containers: Stack[ctk.CTkFrame] = Stack()
+        containers.push(self.general_tab)
 
         annotations = find_all_ui_annotations(self.data_context)
         row_counter = 0
@@ -32,13 +50,21 @@ class TkPropertyPanel(BasePropertyPanel, ctk.CTkFrame):
             for ann in anns:
                 ann_type = type(ann)
 
+                if isinstance(ann, StartSectionAnnotation):
+                    tab = self.tabview.add(ann.name)
+                    tab.grid_columnconfigure(1, weight=1)
+                    containers.push(tab)
+
+                if isinstance(ann, EndSectionAnnotation):
+                    containers.pop()
+
                 if ann_type not in UI_PROPERTY_REGISTRY:
                     logging.warning(f"Annotation not registered: {ann_type.__name__}")
                     continue
 
                 # add property
                 property_field = typing.cast(TkFieldProperty, UI_PROPERTY_REGISTRY[ann_type](ann, model))
-                widgets = property_field.create_widgets(self)
+                widgets = property_field.create_widgets(containers.peek())
 
                 for i, widget in enumerate(widgets):
                     extra = {}
