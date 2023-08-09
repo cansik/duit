a4421d3aea0e59a7107ee1420930b4d24dfff2ac
Florian <github@broox.ch>
2023-08-09 15:42:12 +0200

    added support for checking for events and removing events

--- duit/event/Event.py
+++ duit/event/Event.py
@@ -11,6 +11,12 @@ class Event(Generic[T]):
     def append(self, handler: H) -> None:
         self._handlers.append(handler)
 
+    def remove(self, handler: H) -> None:
+        self._handlers.remove(handler)
+
+    def contains(self, handler: H) -> bool:
+        return handler in self._handlers
+
     def invoke(self, value: T) -> None:
         for handler in self._handlers:
             handler(value)
@@ -30,6 +36,13 @@ class Event(Generic[T]):
     def __iadd__(self, other):
         self.append(other)
         return self
+
+    def __isub__(self, other):
+        self.remove(other)
+        return self
+
+    def __contains__(self, item) -> bool:
+        return self.contains(item)
 
     def __call__(self, value: T):
         self.invoke(value)
--- test/EventTest.py
+++ test/EventTest.py
@@ -6,7 +6,6 @@ from duit.event.Event import Event
 class EventTest(unittest.TestCase):
 
     def test_basic_event(self):
-
         def fire_first(value: int):
             print(f"First: {value}")
 
@@ -18,6 +17,34 @@ class EventTest(unittest.TestCase):
         event += fire_second
 
         event(15)
+
+    def test_check_has_event(self):
+        def fire_first(value: int):
+            print(f"First: {value}")
+
+        def fire_second(value: int):
+            print(f"Second: {value}")
+
+        event = Event[int]()
+        event.append(fire_first)
+        event += fire_second
+
+        assert fire_second in event
+
+    def test_remove_event(self):
+        def fire_first(value: int):
+            print(f"First: {value}")
+
+        def fire_second(value: int):
+            print(f"Second: {value}")
+
+        event = Event[int]()
+        event.append(fire_first)
+        event += fire_second
+
+        event -= fire_second
+
+        assert fire_second not in event
 
 
 if __name__ == '__main__':
