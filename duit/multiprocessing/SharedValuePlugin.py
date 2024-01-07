051cb8ab5f6a2855ea0ba990c3aeac8acd02530e
Florian Bruggisser <github@broox.ch>
2024-01-07 20:47:35 +0100

    added order to shared value plugin

--- duit/multiprocessing/SharedValuePlugin.py
+++ duit/multiprocessing/SharedValuePlugin.py
@@ -1,4 +1,5 @@
 import multiprocessing as mp
+import sys
 from typing import Any
 
 from duit.model.DataField import DataField
@@ -8,6 +9,8 @@ from duit.model.DataFieldPlugin import DataFieldPlugin
 class SharedValuePlugin(DataFieldPlugin):
 
     def __init__(self, shared_value: mp.Value):
+        super().__init__()
+        self.order_index = sys.maxsize  # should always be the last plugin
         self.shared_value = shared_value
 
     def on_set_value(self, field: DataField[Any], old_value: Any, new_value: Any) -> Any:
