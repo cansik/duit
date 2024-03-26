7683ac6830bef0daa7be4cab70b3d6b181c62ac1
Florian Bruggisser <github@broox.ch>
2024-03-27 00:17:56 +0100

    implemente wx options property

--- duit/ui/wx/WxPropertyRegistry.py
+++ duit/ui/wx/WxPropertyRegistry.py
@@ -1,11 +1,13 @@
 from duit.ui.PropertyRegistry import UI_PROPERTY_REGISTRY
 from duit.ui.annotations import NumberAnnotation
 from duit.ui.annotations.BooleanAnnotation import BooleanAnnotation
+from duit.ui.annotations.OptionsAnnotation import OptionsAnnotation
 from duit.ui.annotations.ProgressAnnotation import ProgressAnnotation
 from duit.ui.annotations.SliderAnnotation import SliderAnnotation
 from duit.ui.annotations.TextAnnotation import TextAnnotation
 from duit.ui.wx.properties.BooleanProperty import BooleanProperty
 from duit.ui.wx.properties.NumberProperty import NumberProperty
+from duit.ui.wx.properties.OptionsProperty import OptionsProperty
 from duit.ui.wx.properties.ProgressProperty import ProgressProperty
 from duit.ui.wx.properties.SliderProperty import SliderProperty
 from duit.ui.wx.properties.TextProperty import TextProperty
@@ -17,3 +19,4 @@ def init_wx_registry():
     UI_PROPERTY_REGISTRY[NumberAnnotation] = NumberProperty
     UI_PROPERTY_REGISTRY[SliderAnnotation] = SliderProperty
     UI_PROPERTY_REGISTRY[ProgressAnnotation] = ProgressProperty
+    UI_PROPERTY_REGISTRY[OptionsAnnotation] = OptionsProperty
---
+++ duit/ui/wx/properties/OptionsProperty.py
@@ -0,0 +1,63 @@
+from typing import List, Any
+
+import wx
+
+from duit.model.DataField import DataField
+from duit.ui.annotations.BooleanAnnotation import BooleanAnnotation
+from duit.ui.wx.WxFieldProperty import WxFieldProperty
+
+
+class OptionsProperty(WxFieldProperty[BooleanAnnotation, DataField]):
+    def create_field(self, parent) -> wx.Window:
+        """
+        Create an options field for the given boolean annotation.
+
+        Args:
+            parent: The parent widget.
+
+        Returns:
+            wx.Choice: The created options field.
+        """
+        str_options = [self.get_option_name(o) for o in self.options]
+        field = wx.Choice(parent, choices=str_options)
+        field.Enable(not self.annotation.read_only)
+
+        if self.annotation.tooltip:
+            field.SetToolTip(self.annotation.tooltip)
+
+        def on_dm_changed(value):
+            if value in self.options:
+                index = self.options.index(value)
+                field.SetSelection(index)
+
+        def on_ui_changed(event):
+            selection = event.GetSelection()
+            self.model.value = self.options[selection]
+
+        self.model.on_changed.append(on_dm_changed)
+        field.Bind(wx.EVT_CHOICE, on_ui_changed)
+
+        self.model.fire_latest()
+        return field
+
+    @property
+    def options(self) -> List[Any]:
+        """
+        Get the list of options associated with the annotation.
+
+        Returns:
+            List[Any]: The list of options.
+        """
+        return self.annotation.options
+
+    def get_option_name(self, option) -> str:
+        """
+        Get the name of an option as a string.
+
+        Args:
+            option: The option.
+
+        Returns:
+            str: The name of the option as a string.
+        """
+        return str(option)
