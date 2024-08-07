32c67c6485fbe8d65a31d211194b24c0f4b7f39e
Florian Bruggisser <github@broox.ch>
2024-08-07 15:58:22 +0200

    implemented pygfx based image canvas

--- duit/ui/wx/widgets/WxGfxImageCanvas.py
+++ duit/ui/wx/widgets/WxGfxImageCanvas.py
@@ -1,6 +1,5 @@
 from typing import Optional
 
-import cv2
 import numpy as np
 import pygfx as gfx
 import wx
@@ -11,56 +10,74 @@ class WxGfxImageCanvas(WgpuWidget):
     def __init__(self, parent, image: np.ndarray = None):
         super().__init__(parent)
 
+        self._image: Optional[np.ndarray] = image
+
+        self._update_texture_requested: bool = False
+        self._update_projection_requested: bool = False
+
         self.renderer = gfx.renderers.WgpuRenderer(self)
         self.scene = gfx.Scene()
         self.camera = gfx.OrthographicCamera()
 
         self.texture: Optional[gfx.Texture] = None
-        self.geometry: Optional[gfx.Geometry] = None
-        self.material: Optional[gfx.Material] = None
-        self.mesh: Optional[gfx.Mesh] = None
+        self.gfx_image: Optional[gfx.Image] = None
 
         self.Bind(wx.EVT_SIZE, self.on_size)
 
+        self.request_draw(self._animate)
+
     @property
-    def image(self) -> np.ndarray:
-        return self.texture.image
+    def image(self) -> Optional[np.ndarray]:
+        return self._image
 
     @image.setter
-    def image(self, image: np.ndarray):
-        h, w = image.shape[:2]
+    def image(self, image: Optional[np.ndarray]):
+        self._image = image
+        self._update_texture_requested = True
+
+    def _animate(self):
+        if self._update_texture_requested:
+            self._update_texture()
+            self._update_texture_requested = False
+
+        if self._update_projection_requested:
+            self._update_projection()
+            self._update_projection_requested = False
+
+        self.renderer.render(self.scene, self.camera)
+        self.request_draw()
+
+    def _update_texture(self):
+        if self._image is None:
+            if self.gfx_image is not None:
+                self.scene.remove(self.gfx_image)
+                self.gfx_image = None
+                self.texture = None
+            return
+
+        h, w = self._image.shape[:2]
 
         if self.texture is None:
-            self.texture = gfx.Texture(image, dim=2)
+            self.texture = gfx.Texture(self._image, dim=2)
 
-        if self.geometry is None:
-            self.geometry = gfx.plane_geometry(w, h)
+        if self.gfx_image is None:
+            self.gfx_image = gfx.Image(
+                gfx.Geometry(grid=self.texture),
+                gfx.ImageBasicMaterial(clim=(0, 255)),
+            )
 
-        if self.material is None:
-            self.material = gfx.MeshBasicMaterial(map=self.texture)
+            self.scene.add(self.gfx_image)
 
-        if self.mesh is None:
-            self.mesh = gfx.Mesh(self.geometry, self.material)
-            self.scene.add(self.mesh)
+            self.camera.show_object(self.scene, view_dir=(0, 0, -1))
+            self.camera.local.scale_y = -1
 
-        self.texture.image = image
-        self.texture.update_range((0, 0, 0), (1, 1, 1))
-        self.geometry = gfx.plane_geometry(1, h / w)
-        self.mesh.geometry = self.geometry
-
-        self.refresh_scene()
-
-    def refresh_scene(self):
-        def update():
-            self.renderer.render(self.scene, self.camera)
-            self.request_draw()
-
-        wx.CallAfter(update)
+        self.texture.data[:] = self._image[:]
+        self.texture.update_range((0, 0, 0), (w, h, 1))
 
     def on_size(self, event):
-        wx.CallAfter(self.update_projection)
+        self._update_projection_requested = True
 
-    def update_projection(self):
+    def _update_projection(self):
         size = self.GetClientSize()
         aspect_ratio = size.width / size.height
         self.camera.left = -size.width / 2
@@ -68,4 +85,3 @@ class WxGfxImageCanvas(WgpuWidget):
         self.camera.top = size.height / 2
         self.camera.bottom = -size.height / 2
         self.camera.update_projection_matrix()
-        self.refresh_scene()
