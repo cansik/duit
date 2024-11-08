8ea92c6fa8cee73d9f174af71bc2fe33b3158219
Florian Bruggisser <github@broox.ch>
2024-11-08 18:09:44 +0100

    added high dpi enable util for wx

--- duit/ui/wx/WxUtils.py
+++ duit/ui/wx/WxUtils.py
@@ -1,3 +1,27 @@
+import os
 
 MAX_INT32 = 2 ** 31 - 1
 MIN_INT32 = 2 ** 31 * -1
+
+
+def enable_high_dpi_mode():
+    if os.name != "nt":
+        return
+
+    try:
+        from ctypes import OleDLL
+
+        # Turn on high-DPI awareness to make sure rendering is sharp on big
+        # monitors with font scaling enabled.
+        OleDLL('shcore').SetProcessDpiAwareness(1)
+
+    except AttributeError:
+        pass
+
+    except OSError:
+        # exc.winerror is often E_ACCESSDENIED (-2147024891/0x80070005).
+        # This occurs after the first run, when the parameter is reset in the
+        # executable's manifest and then subsequent calls raise this exception
+        # See last paragraph of Remarks at
+        # [https://msdn.microsoft.com/en-us/library/dn302122(v=vs.85).aspx](https://msdn.microsoft.com/en-us/library/dn302122(v=vs.85).aspx)
+        pass
