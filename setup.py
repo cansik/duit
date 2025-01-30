c38e4e3ae3689e51344fa4e70e94ef352efe75b1
Florian Bruggisser <github@broox.ch>
2025-01-30 09:47:41 +0100

    bumped package version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from typing import List
 from setuptools import setup, find_packages
 
 PACKAGE_NAME = "duit"
-PACKAGE_VERSION = "0.1.15.0"
+PACKAGE_VERSION = "0.1.16"
 PACKAGE_URL = "https://github.com/cansik/duit"
 
 PACKAGE_DOC_MODULES = ["duit", "!duit.vision"]
