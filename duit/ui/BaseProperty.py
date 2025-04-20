9d11945c5ea7b15a03f767181f60871439a0faed
Florian Bruggisser <github@broox.ch>
2025-04-20 23:07:57 +0200

    implemented basic silent contextmanager in BaseProperty

--- duit/ui/BaseProperty.py
+++ duit/ui/BaseProperty.py
@@ -1,4 +1,6 @@
+import threading
 from abc import ABC, abstractmethod
+from contextlib import contextmanager
 from typing import Optional, Any, Iterable, TypeVar, Generic
 
 from duit.model.DataField import DataField
@@ -9,6 +11,16 @@ M = TypeVar("M", bound=DataField)
 
 
 class BaseProperty(Generic[T, M], ABC):
+    """
+    Abstract base class representing a generic property with an associated UI annotation and data model.
+
+    This class provides the structure for managing UI-bound properties with optional underlying models.
+    It supports thread-safe operations and requires implementation of a widget creation method.
+
+    :param annotation: The UIAnnotation associated with this property.
+    :param model: An optional DataField model associated with the property.
+    """
+
     def __init__(self, annotation: T, model: Optional[M] = None):
         """
         Initialize a BaseProperty.
@@ -19,6 +31,8 @@ class BaseProperty(Generic[T, M], ABC):
         self.annotation = annotation
         self.model = model
 
+        self._silent_lock = threading.Lock()
+
     @abstractmethod
     def create_widgets(self, *args) -> Iterable[Any]:
         """
@@ -27,6 +41,25 @@ class BaseProperty(Generic[T, M], ABC):
         This method should be implemented by subclasses to define the specific
         widgets that make up the property. The widgets can be of any type.
 
-        :return: An iterable of widgets representing the property.
+        :returns: An iterable of widgets representing the property.
         """
         pass
+
+    @contextmanager
+    def silent(self):
+        """
+        Context manager to temporarily suppress updates or notifications.
+
+        This is useful for batching operations where intermediate updates are not needed.
+        Ensures thread-safe entry and exit using a non-blocking lock.
+
+        :yields: True if the lock was acquired and operations are silent, otherwise False.
+        """
+        acquired = self._silent_lock.acquire(blocking=False)
+        if not acquired:
+            yield False
+            return
+        try:
+            yield True
+        finally:
+            self._silent_lock.release()
